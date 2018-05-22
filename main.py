8e1758390a5a837e9e3c94dd2909b7ce9874dd59
agorlenko <agorlenko1@gmail.com>
2018-05-23 01:39:20 +0300

    test

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://whatsnew1cbot.herokuapp.com/' + TOKEN)
+    bot.set_webhook(url='https://whatsnew1cbot.herokuapp.com/bot')
     return "!", 200
 
 if __name__ == "__main__":
