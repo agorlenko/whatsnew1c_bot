178abe958e33cd2d93375cc1d95b252e0fb42868
agorlenko <agorlenko1@gmail.com>
2018-05-23 03:51:14 +0300

    test

--- main.py
+++ main.py
@@ -44,10 +44,10 @@ def getMessage():
 
 @server.route("/")
 def webhook():
-    print('webhook!')
+    raise 'HOOK'
     bot.remove_webhook()
     bot.set_webhook(url='https://whatsnew1cbot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 #if __name__ == "__main__":
-server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 8000)))
+server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
