50e23055e7dbc00da08d60a97ebdb6a5a0a3b65e
agorlenko <agorlenko1@gmail.com>
2018-05-23 04:01:27 +0300

    test

--- main.py
+++ main.py
@@ -50,4 +50,6 @@ def webhook():
     return "!", 200
 
 if __name__ == "__main__":
+    webhook()
+    print('running server')
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
